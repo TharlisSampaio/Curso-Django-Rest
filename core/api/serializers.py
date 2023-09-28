6f72b239adaf3404f3c7914a2e95992faef2e048
Tharlis Sampaio <tharlis.sampaio@outlook.com>
2023-09-27 22:22:01 -0500

    ralações ManyToMany e Foreignkey

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,19 +1,45 @@
 from rest_framework.serializers import ModelSerializer
-from rest_framework.fields import SerializerMethodField
 from core.models import PontoTuristico
+from atracoes.models import Atracao
 from atracoes.api.serializers import AtracoesSerializer
 from enderecos.api.serializers import EnderecoSerializer
+from enderecos.models import Endereco
 
 
 class PontoTuristicoSerializer(ModelSerializer):
-    atracoes = AtracoesSerializer(many=True)
-    endereco = EnderecoSerializer()
-    descricao_completa = SerializerMethodField()
+    # "This list may not be empty." que dizer que o campo é obrigatorio
+    atracoes = AtracoesSerializer(many=True)  # read_only=True tira a obrigaroriadade
+    endereco = EnderecoSerializer()  # campo relacionado
+    # comentarios = ComentarioSerializer()
+    # descricao_completa = SerializerMethodField()
 
     class Meta:
         model = PontoTuristico
         fields = ('id', 'nome', 'descricao', 'aprovado', 'foto',
-                  'atracoes', 'comentarios', 'endereco', 'descricao_completa')
+                  'atracoes', 'comentarios', 'endereco', 'avaliacoes')
+        read_only_fields = ('comentarios', 'avaliacoes')  # campos como somente leitura
+
+    def cria_atracoes(self, atracoes, ponto):
+        for atracao in atracoes:
+            at = Atracao.objects.create(**atracao)
+            ponto.atracoes.add(at)
+
+    def create(self, validated_data):
+        atracoes = validated_data['atracoes']  # acessa a lista de atrações através da chave 'atracoes'
+        del validated_data['atracoes']  # del palavra-chave é usada para excluir objetos.
+
+        endereco = validated_data['endereco']
+        del validated_data['endereco']
+
+        ponto = PontoTuristico.objects.create(**validated_data)
+        self.cria_atracoes(atracoes, ponto)
+
+        end = Endereco.objects.create(**endereco)
+        ponto.endereco = end
+
+        ponto.save()
+
+        return ponto
 
     def get_descricao_completa(self, obj):
         return '%s - %s' % (obj.nome, obj.descricao)
